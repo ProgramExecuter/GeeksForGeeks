6c7e3641446bf2e486490821a117e1e13054890d
Amit Singh Rana <ranaa73000@gmail.com>
2021-09-08 10:31:20 +0530

    'Delete keys in a Linked list' solution Added

---
+++ Medium/Delete_keys_in_a_Linked_list.cpp
@@ -0,0 +1,131 @@
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+  int data;
+  struct Node *next;
+
+  Node(int x)
+  {
+    data = x;
+    next = NULL;
+  }
+};
+
+void append(struct Node **head_ref, struct Node **tail_ref,
+            int new_data)
+{
+  struct Node *new_node = new Node(new_data);
+  if (*head_ref == NULL)
+    *head_ref = new_node;
+  else
+    (*tail_ref)->next = new_node;
+  *tail_ref = new_node;
+}
+
+/* Function to get the middle of the linked list*/
+struct Node *deleteAllOccurances(struct Node *head, int);
+
+void printList(Node *head)
+{
+  while (head != NULL)
+  {
+    cout << head->data << " ";
+    head = head->next;
+  }
+  cout << "\n";
+}
+int sizeofthelinkedlist(Node *head)
+{
+  int x = 0;
+  while (head != NULL)
+  {
+    x++;
+    head = head->next;
+  }
+  return x;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+  int T, i, n, l;
+
+  // TO BE REMOVED
+  for (int i = 0; i < 2000; i++)
+    ;
+
+  cin >> T;
+
+  while (T--)
+  {
+    struct Node *head = NULL, *tail = NULL;
+
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+      cin >> l;
+      append(&head, &tail, l);
+    }
+
+    int kk;
+    cin >> kk;
+    head = deleteAllOccurances(head, kk);
+    cout << sizeofthelinkedlist(head) << endl;
+    printList(head);
+  }
+  return 0;
+}
+
+// } Driver Code Ends
+
+/* Link list Node 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};*/
+/*You are required to complete below method*/
+Node *deleteAllOccurances(Node *head, int x)
+{
+  //Your code here
+  while (head && head->data == x)
+  {
+    Node *del = head;
+    head = head->next;
+    delete del;
+  }
+  if (!head)
+    return NULL;
+  if (!head->next && head->data == x)
+    return NULL;
+  if (!head->next && head->data != x)
+    return head;
+  Node *temp = head->next, *prev = head;
+  while (temp)
+  {
+    if (temp->data == x)
+    {
+      Node *del = temp;
+      temp = temp->next;
+      prev->next = temp;
+      delete del;
+    }
+    else
+    {
+      prev = temp;
+      temp = temp->next;
+    }
+  }
+  return head;
+}
